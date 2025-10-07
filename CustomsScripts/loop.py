4afcf13b498662ae75e8e4f8a2269fe0508de412
GuillaumeGarcia <garciaguillaumeunivlille@gmail.com>
2025-10-07 11:13:29 +0200

    loop index refacto

--- CustomsScripts/loop.py
+++ CustomsScripts/loop.py
@@ -1,13 +1,5 @@
-# MatCubeTest
-
-
-"""
-A simple example of using pyroomacoustics to simulate
-sound propagation in a shoebox room and record the result
-to a wav file.
-"""
 import json
-import os
+import os, sys
 import time
 import numpy as np
 import pyroomacoustics as pra
@@ -49,11 +41,11 @@ class Timer:
         print(f"{fromText} elapsed time : {elapsed_time:0.4f} seconds")
 
 
-def exportIRToWav(formattedIRs, norm, fileName, iMic):
+def exportIRToWav(computedIRs, norm, fileName, micIndex):
 
-    path = f"{folderpath}/{fileName}.wav"
+    path = f"{folderpath}/{fileName}"
 
-    signal = formattedIRs[iMic][0]  # [micro][source]
+    signal = computedIRs[micIndex][0]  # [micro][source]
 
     if norm:
         from utilities import normalize
@@ -72,26 +64,41 @@ def exportIRToWav(formattedIRs, norm, fileName, iMic):
 JSONData = {}
 
 
-def makeJsonData(signal, name, i, showGraph):
+def makeJsonData(signal, name, wavName, sLabel, sourcePos, micPos, showGraph):
 
     # Get volume RootMinSquare sqr(avg(all [i]²))
     volume = pra.rms(signal)
     # maxAmp = np.max(pra.doa.detect_peaks(signal))
 
     # Get Source-Mic distance
-    m = microphonesMap.get(i + 1)
-    s = TheatreRoom.sources[0].position
-    dist = np.linalg.norm(s - m)
+    dist = np.linalg.norm(np.array(sourcePos) - np.array(micPos))
 
     # detection of first ""peak"" > 0.000000
     p = pra.doa.detect_peaks(signal, mph=0.000001, show=showGraph)
-    firstPeakIndex = p[0] if len(p) else 0
+    if len(p):
+        firstPeakIndex = max(p)
+    else:
+        firstPeakIndex = 0
+
     peakDelay = firstPeakIndex / fs
 
+    comp = compareLawfulVolume(dist, volume)
+
     irData = {
-        "distance": float(dist),
-        "volume": float(volume),
-        "peakDelay": float(peakDelay),
+        "IRPath": wavName,
+        "distance": truncate(dist, 6),
+        "volume": truncate(volume, 6),
+        "lawfulVolume": truncate(comp[0], 5),
+        "volumeGap": truncate(comp[1], 5),
+        "peakDelay": truncate(peakDelay, 5),
+        "sourceID": sLabel,
+        "sourceX": sourcePos[0],
+        "sourceY": sourcePos[1],
+        "sourceZ": sourcePos[2],
+        "micID": str(i + 1),
+        "micX": micPos[0],
+        "micY": micPos[1],
+        "micZ": micPos[2],
     }
     JSONData.update({name: irData})
 
@@ -108,6 +115,33 @@ def writeJsonFile():
         outfile.write(data + "\n")
 
 
+def compareLawfulVolume(distance, volume):
+    supposedVolume = 1 / pow(distance, 2)
+    gap = abs(supposedVolume - volume)
+    return [supposedVolume, gap]
+
+
+def truncate(f, n):
+    """Truncates/pads a float f to n decimal places without rounding"""
+    s = "{}".format(f)
+    if "e" in s or "E" in s:
+        return "{0:.{1}f}".format(f, n)
+    i, p, d = s.partition(".")
+    return ".".join([i, (d + "0" * n)[:n]])
+
+
+def writeRawIRs(jdata, filename):
+
+    #  encode dict as JSON
+    data = json.dumps(jdata, indent=1, ensure_ascii=True)
+    #  set output path and file name (set your own)
+
+    #  write JSON file
+    filepath = f"{filename}.txt"
+    with open(filepath, "w") as outfile:
+        outfile.write(data + "\n")
+
+
 # Mesh Import
 try:
     from stl import mesh
@@ -120,12 +154,14 @@ except ImportError as err:
 
 # fs is samplerate as integer
 # audio source is numpy.ndarray
-# fs, anechoicAudioSource = wavfile.read("../examples/samples/guitar_16k.wav")
-fs, anechoicAudioSource = wavfile.read("../examples/CustomSamples/Basic-808-Clap.wav")
+fs, anechoicAudioSource = wavfile.read(
+    "../examples/CustomSamples/IR-Dirac-44100-20hz-22050hz-1s.wav"
+)
 
 size_reduc_factor = 1  # to get a realistic room size (not 3km)
 allMeshesGeometry = []
 mat = pra.Material(energy_absorption=1.0, scattering=0.0)
+# mat = pra.Material(energy_absorption=0.01, scattering=0.6) //Briques peintes
 materials = [
     mat,
     mat,
@@ -155,66 +191,63 @@ TheatreRoom = pra.Room(
     walls=allMeshesGeometry,
     fs=fs,
     max_order=3,
-    ray_tracing=True,
+    ray_tracing=False,
     air_absorption=True,
 )
 
 # sources/mic locations
 sourcesMap = {
-    "A": [-1.7500, 9.1500, 3.3572],
-    "B": [1.7500, 9.1500, 3.3572],
-    "C": [3.0000, 2.0000, 3.3572],
-    "D": [-3.0000, 2.0000, 3.3572],
-    "E": [-3.3500, -1.0000, 1.4000],
-    "F": [0.0000, -1.0000, 1.4000],
-    "G": [3.3500, -1.0000, 1.4000],
+    "A": [-1.75, 9.15, 3.3572],
+    # "B": [1.75, 9.15, 3.3572],
+    # "C": [3.0, 2.0, 3.3572],
+    # "D": [-3.0, 2.0, 3.3572],
+    "E": [-3.35, -1.0, 1.4],
+    # "F": [0.0, -1.0, 1.4],
+    # "G": [3.35, -1.0, 1.4],
 }
 microphonesMap = {
-    1: [-3.8, -3.7500, 1.3],
-    2: [3.8, -3.7500, 1.3],
-    3: [2.4077, -7.3239, 1.3],
-    4: [-2.4077, -7.3239, 1.3],
-    5: [-5.0, -2.0, 3.5],
-    6: [5.0, -2.0, 3.5],
-    7: [3.5, -8.2, 3.5],
-    8: [-3.5, -8.2, 3.5],
-    9: [-5.1, -2.0, 5.8],
-    10: [5.1, -2.0, 5.8],
+    1: [-3.8, -3.75, 1.3],
+    # 2: [3.8, -3.75, 1.3],
+    # 3: [2.4077, -7.3239, 1.3],
+    # 4: [-2.4077, -7.3239, 1.3],
+    # 5: [-5.0, -2.0, 3.5],
+    # 6: [5.0, -2.0, 3.5],
+    # 7: [3.5, -8.2, 3.5],
+    # 8: [-3.5, -8.2, 3.5],
+    # 9: [-5.1, -2.0, 5.8],
+    # 10: [5.1, -2.0, 5.8],
     11: [4.0, -8.5, 5.8],
-    12: [-4.0, -8.5, 5.8],
-    13: [-5.1, -2.0, 8.2],
-    14: [5.1, -2.0, 8.2],
-    15: [4.0, -8.5, 8.2],
-    16: [-4.0, -8.5, 8.2],
+    # 12: [-4.0, -8.5, 5.8],
+    # 13: [-5.1, -2.0, 8.2],
+    # 14: [5.1, -2.0, 8.2],
+    # 15: [4.0, -8.5, 8.2],
+    # 16: [-4.0, -8.5, 8.2],
 }
 
-
 # Making pyroom mic array from the map, to add them in the room
-microphoneArray = []
-for i in range(1, len(microphonesMap) + 1):
-    microphoneArray.append(microphonesMap[i])
-
-# microphoneArray = [(k, sum(v)) for k, v in microphonesMap.items()]
-
+microphoneArray = list(microphonesMap.values())  # [[mx,my,mz],[mx,my,mz]...]
 TheatreRoom.add_microphone_array(
-    pra.MicrophoneArray(np.array(microphoneArray).T, TheatreRoom.fs)
+    pra.MicrophoneArray(
+        R=np.array(object=microphoneArray).T,
+        fs=TheatreRoom.fs,
+        directivity=pra.directivities.Omnidirectional(),
+    )
 )
 
+# Render folder
 folderpath = f"./IRtest"
 if not os.path.exists(folderpath):
     os.makedirs(folderpath)
 
-
 t = Timer()
 t.start()
 
-
-# Sources Loop
-for sourceLabel, pos in sourcesMap.items():
+# Main Loop
+for sourceLabel, sourcePos in sourcesMap.items():
 
     if TheatreRoom.sources:
         TheatreRoom.sources.clear()
-    TheatreRoom.add_source(pos, signal=anechoicAudioSource)
+    TheatreRoom.add_source(sourcePos, signal=anechoicAudioSource)
 
     if TheatreRoom.rir:
         TheatreRoom.rir.clear()
@@ -223,45 +256,65 @@ for sourceLabel, pos in sourcesMap.items():
 
     # This function will generate all the images sources up to the order required and use them to generate the RIRs, which will be stored in the rir attribute of room.
     TheatreRoom.image_source_model()
-    t.show("ImageSource")
+    t.show(f"--ImageSource for source {sourceLabel}--")
+
+    if TheatreRoom.ray_tracing:
+        TheatreRoom.ray_tracing()
+        t.show(f"--RayTracing for source {sourceLabel}--")
 
     # TheatreRoom.plot_rir()
     TheatreRoom.simulate()
     # t.show("Plot-RIR")
-    t.show('simulate')
+    t.show(f"--Simulate sound with source {sourceLabel}--")
 
     # The attribute rir is a list of lists so that the outer list is on microphones and the inner list over sources.
-    allRawIRs = TheatreRoom.rir
-    # required conversion from list to nparray
-    formattedIRs = np.asarray(allRawIRs, dtype="object")
+    computedIRs = TheatreRoom.rir
 
     # Create a plot
     plt.figure()
 
-    # mic loop through each IR
-    if len(microphoneArray) == len(formattedIRs):
-        for i in range(0, len(formattedIRs)):
+    # mic loop through each computedIR : [[micro][source]]
+    micLoopIndex = 0  # needed since we iterate from a map/dict with arbitrary int IDs
+    if len(computedIRs) == len(microphoneArray):
+        for micID, micPos in microphonesMap.items():
 
-            fileName = f"{sourceLabel}-{i+1}"
+            fileName = f"{sourceLabel}{micID}"
+            wavFileName = f"{fileName}.wav"
 
             signal = exportIRToWav(
-                formattedIRs=formattedIRs, norm=False, fileName=fileName, iMic=i
+                computedIRs=computedIRs,
+                norm=False,
+                fileName=wavFileName,
+                micIndex=(micLoopIndex),
             )
 
-            # store json data
-            makeJsonData(signal, fileName, i, showGraph=False)
+            print(sourcePos)
+            print(micPos)
 
-                # plot signal at microphone 1
-            plt.subplot(len(microphoneArray), 1, i+1)
+            t.show(f"Export {wavFileName} {i}/{len(computedIRs)}")
+
+            # store json data
+            makeJsonData(
+                signal,
+                fileName,
+                wavFileName,
+                sourceLabel,
+                sourcePos,
+                micPos,
+                showGraph=False,
+            )
+
+            # plot signal at microphone 1
+            plt.subplot(len(microphoneArray), 1, i + 1)
             plt.plot(TheatreRoom.mic_array.signals[i])
             plt.title(f"Microphone {i} signal")
             plt.xlabel("Time [s]")
- 
+            micLoopIndex += 1
+    else:
+        raise Exception(f"IR data is missing some Microphones indexes")
+
     plt.savefig(f"{folderpath}/{sourceLabel}.png")
 
 writeJsonFile()
-
-
-t.show("Wav Export")
+t.show("Json Export")
 t.stop()
-
